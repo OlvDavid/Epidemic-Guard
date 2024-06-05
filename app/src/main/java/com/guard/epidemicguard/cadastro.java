3958cb7dd440c6cfd3191d68eef62aba4414df5f
OlvDavid <146491742+OlvDavid@users.noreply.github.com>
2024-06-05 14:18:59 -0300

    Tela de Cadastro e navegação basica

--- app/src/main/java/com/guard/epidemicguard/cadastro.java
+++ app/src/main/java/com/guard/epidemicguard/cadastro.java
@@ -10,22 +10,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class login extends AppCompatActivity {
+public class cadastro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.telaLogin), (v, insets) -> {
+        setContentView(R.layout.cadastro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.telaCadastro), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
 
-    public void cadastrar(View v){
-        Intent i = new Intent(this, cadastro.class);
+    public void voltar(View v){
+        Intent i = new Intent(this, login.class);
         startActivity(i);
     }
 }
