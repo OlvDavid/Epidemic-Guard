fc518971ebfd4ded70f13c5bea334c9027dac613
OlvDavid <146491742+OlvDavid@users.noreply.github.com>
2024-06-06 02:55:38 -0300

    Sistema de Login Completo

--- app/src/main/java/com/guard/epidemicguard/cadastro.java
+++ app/src/main/java/com/guard/epidemicguard/cadastro.java
@@ -1,31 +1,160 @@
 package com.guard.epidemicguard;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class cadastro extends AppCompatActivity {
+
+    private EditText editEmail, editSenha, editNome, editCPF;
+    private Button btnCadastrar;
+    FirebaseAuth usuario = FirebaseAuth.getInstance();
+
+    String[] mensagens = {"Preencha todos os campos", "Cadastro realizado com sucesso"};
+    String usuarioID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.cadastro);
+        FirebaseApp.initializeApp(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.telaCadastro), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        inicarComponentes();
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = editNome.getText().toString();
+                String email = editEmail.getText().toString();
+                String senha = editSenha.getText().toString();
+                String cpf = editCPF.getText().toString();
+
+                if(nome.isEmpty() || email.isEmpty() || senha.isEmpty() || cpf.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else{
+                    cadastrarUsuario(v);
+                }
+            }
+        });
+    }
+    private void cadastrarUsuario(View v){
+
+        String email = editEmail.getText().toString();
+        String senha = editSenha.getText().toString();
+
+        usuario.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+
+                    salvarDadosUsuario();
+
+                    Snackbar snackbar = Snackbar.make(v, mensagens[1], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else{
+                    String erro;
+                    try {
+                        throw task.getException();
+
+                    }catch (FirebaseAuthWeakPasswordException e) {
+                        erro = "Digite uma senha com no mínimo 6 caracteres";
+                    }catch (FirebaseAuthUserCollisionException e) {
+                        erro = "Esta conta já esta cadastrada";
+                    }catch (FirebaseAuthInvalidCredentialsException e) {
+                        erro = "Digite um e-mail válido";
+                    }catch (Exception e){
+                        erro = "Erro ao cadastrar usuário";
+                    }
+                    //Toast.makeText(cadastro.this, erro, Toast.LENGTH_SHORT).show();
+                    Snackbar snackbar = Snackbar.make(v, erro, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+                }
+            }
+        });
+    }
+
+    private void salvarDadosUsuario(){
+        String nome = editNome.getText().toString();
+        String cpf = editCPF.getText().toString();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String,Object> usuarios = new HashMap<>();
+        usuarios.put("nome", nome);
+        usuarios.put("CPF", cpf);
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuários").document(usuarioID);
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d("db","Sucesso ao salvar os dados");
+            }
+        }).
+         addOnFailureListener(new OnFailureListener() {
+             @Override
+             public void onFailure(@NonNull Exception e) {
+                 Log.d("db_error", "Erro ao salvar os dados" + e.toString());
+             }
+         });
+
     }
 
     public void voltar(View v){
         Intent i = new Intent(this, login.class);
         startActivity(i);
     }
+
+    private void inicarComponentes(){
+        editNome = findViewById(R.id.editNome);
+        editEmail = findViewById(R.id.editEmail);
+        editSenha = findViewById(R.id.editSenha);
+        editCPF = findViewById(R.id.editCpf);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+
+    }
+
 }
--- app/src/main/java/com/guard/epidemicguard/login.java
+++ app/src/main/java/com/guard/epidemicguard/login.java
@@ -1,16 +1,36 @@
 package com.guard.epidemicguard;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class login extends AppCompatActivity {
+
+    private Button btnCadastrar, btnEntrar;
+    private TextInputEditText editEmail, editSenha;
+    private ProgressBar progressBar;
+
+    String[] mensagens = {"Preencha todos os campos",};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +42,87 @@ public class login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        iniciarComponetes();
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = editEmail.getText().toString();
+                String senha = editSenha.getText().toString();
+
+                if(email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else{
+                    autenticarUsuario(v);
+                }
+            }
+        });
+    }
+
+    private void autenticarUsuario(View v){
+        String email = editEmail.getText().toString();
+        String senha = editSenha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            telaPrincipal();
+                        }
+                    }, 3000);
+                }else{
+                    String erro;
+                    try{
+                        throw task.getException();
+                    }catch (Exception e){
+                        erro = "Erro ao logar usuário";
+                    }
+                    Snackbar snackbar = Snackbar.make(v, erro, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser usuarioAtual = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(usuarioAtual != null){
+            telaPrincipal();
+        }
+    }
+
+    private void telaPrincipal(){
+        Intent i = new Intent(this, menu.class);
+        startActivity(i);
+        finish();
     }
 
     public void cadastrar(View v){
         Intent i = new Intent(this, cadastro.class);
         startActivity(i);
     }
+
+    private void iniciarComponetes(){
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+        editEmail = findViewById(R.id.editEmail);
+        editSenha = findViewById(R.id.editSenha);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        progressBar = findViewById(R.id.progressBar);
+    }
 }
+
