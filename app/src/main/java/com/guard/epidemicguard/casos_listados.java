a59a3774518a4bad4b4c6f6a15304af22be3ef5b
OlvDavid <146491742+OlvDavid@users.noreply.github.com>
2024-06-11 23:39:50 -0300

    Total de casos na tela de casos listados

--- app/src/main/java/com/guard/epidemicguard/casos_listados.java
+++ app/src/main/java/com/guard/epidemicguard/casos_listados.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class casos_listados extends AppCompatActivity {
 
     private ImageView imageVoltarLista;
+
+    private TextView textTotalCasos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +42,29 @@ public class casos_listados extends AppCompatActivity {
                 finish();
             }
         });
+        totalCasos();
+    }
+
+    private void totalCasos(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("Casos").get().addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                QuerySnapshot snapshots = task.getResult();
+                if(snapshots != null){
+                    int total = snapshots.size();
+                    textTotalCasos.setText("Total de casos: " + total);
+                }else {
+                    textTotalCasos.setText("Total de casos: 0");
+                }
+            }else{
+                textTotalCasos.setText("Erro ao carregar dados");
+            }
+        });
     }
 
     private void iniciarComponentes(){
         imageVoltarLista = findViewById(R.id.imageVoltarLista);
+        textTotalCasos = findViewById(R.id.textTotalCasos);
     }
 }
--- app/src/main/java/com/guard/epidemicguard/menu.java
+++ app/src/main/java/com/guard/epidemicguard/menu.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,9 +15,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class menu extends AppCompatActivity {
 
     private ImageView perfil, imageMapa, imageNovoCaso, imageGuia, imageLista;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
